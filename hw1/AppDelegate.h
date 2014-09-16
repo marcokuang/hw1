59e1cd69050fcd9e0495834618937777bbc52043
ABC <ABC@abc.gateway.pace.com>
2014-09-16 16:37:12 -0700

    Initial Commit

---
+++ hw1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  hw1
+//
+//  Created by ABC on 9/16/14.
+//  Copyright (c) 2014 marcok. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ hw1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  hw1
+//
+//  Created by ABC on 9/16/14.
+//  Copyright (c) 2014 marcok. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
